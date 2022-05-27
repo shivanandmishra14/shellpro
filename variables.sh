c8a519658e4d57a0f41652984b61ab40cff6338b
SHIVANAND <shivanandmishra14@gmail.com>
2022-05-27 07:03:24 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# This is a comment
+# echo "Hello worls"  # new comments
+
+
+
+#  Variables are the container which store some kind of data 
+#  two type os variables system and user defined 
+#  System is defined by system and user defined are created by user. 
+#  system variables are in CAP and user definded are generally in small but not hard rule
+# should be only in small.
+
+echo $BASH 
+echo $BASH_VERSION
+echo $HOME
+echo my directory is $PWD
+
+# user defined variables
+
+name=Shiva
+echo my name is $name
+#like this you can use userdefined variabled with dollar symbol          
